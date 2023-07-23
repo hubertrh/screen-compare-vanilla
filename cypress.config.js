d973c410ad79f22532d3367cbacf700df79e200e
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-07-23 18:05:50 +0100

    Edit Cypress config and scripts

--- cypress.config.js
+++ cypress.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: `http://localhost:${5173 || 4173}`,
+    baseUrl: `http://localhost:4173`,
     viewportWidth: 1280,
     viewportHeight: 680,
     supportFile: "cypress/support/e2e.{js,jsx,ts,tsx}",
-    videoUploadOnPasses: true,
+    videoUploadOnPasses: false,
   },
 });
---
+++ cypress/e2e/cookies.cy.js
@@ -0,0 +1,27 @@
+describe("Cookie Consent Banner", () => {
+  beforeEach(() => {
+    cy.clearCookies();
+    cy.visit("http://localhost:4173");
+  });
+
+  it("Cookie consent banner and buttons should be displayed if no cookiesAccepted cookie is set", () => {
+    cy.get(".cookie-consent").should("not.have.class", "invisible");
+    cy.get(".cookie-consent-backdrop").should("not.have.class", "invisible");
+    cy.get(".cookie-consent__button--accept").should("be.visible");
+    cy.get(".cookie-consent__button--reject").should("be.visible");
+  });
+
+  it("Accept cookies button should set a cookiesAccepted cookie and hide the banner", () => {
+    cy.get(".cookie-consent__button--accept").click();
+    cy.getCookie("cookiesAccepted").should("have.property", "value", "true");
+    cy.get(".cookie-consent").should("have.class", "invisible");
+    cy.get(".cookie-consent-backdrop").should("have.class", "invisible");
+  });
+
+  it("Reject cookies button should set a cookiesAccepted cookie to false and hide the banner", () => {
+    cy.get(".cookie-consent__button--reject").click();
+    cy.getCookie("cookiesAccepted").should("have.property", "value", "false");
+    cy.get(".cookie-consent").should("have.class", "invisible");
+    cy.get(".cookie-consent-backdrop").should("have.class", "invisible");
+  });
+});
